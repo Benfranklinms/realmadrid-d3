98cd8f3adcc4a286a635895376b4f01d1bc59313
Benfranklin m s <bfranklinms@gmail.com>
2024-11-06 00:17:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toDark() {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+  }
+  function toLight() {
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+  }
